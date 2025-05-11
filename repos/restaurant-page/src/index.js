64142b32ba355091c64d5e0a462ec208b344302e
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2025-05-11 09:10:48 -0400

    Add menu tab click handler to load menu content

--- repos/restaurant-page/src/index.js
+++ repos/restaurant-page/src/index.js
@@ -1,1 +1,19 @@
-document.getElementById("content").textContent = "Hello from JavaScript!";
+import loadMenu from "./menu";
+import loadHome from "./home";
+
+const content = document.getElementById("content");
+content.textContent = "";
+content.appendChild(loadHome());
+
+document.getElementById("home-tab").addEventListener("click", () => {
+content.textContent= "";
+content.appendChild(loadHome());
+});
+
+document.getElementById("menu-tab").addEventListener("click", () => {
+content.textContent= "";
+content.appendChild(loadMenu());
+});
+
+
+
